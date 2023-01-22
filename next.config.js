93f6b5abfd3a5523f3d5870094ef6a1ac7dee4ff
muzyk0 <ru9art@gmail.com>
2023-01-22 20:02:26 +0300

    create guild route and redirect to notion

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/guild',
+        destination: 'https://muzyk0.notion.site/84a114923faf4cb286756e8cb6abca23',
+        permanent: false
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
