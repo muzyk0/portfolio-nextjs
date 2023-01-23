a20a484691cc7963f49702d75d383951e64dd416
muzyk0 <ru9art@gmail.com>
2023-01-23 11:11:31 +0300

    create guild route and redirect to notion

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
         source: '/guild',
         destination: 'https://muzyk0.notion.site/84a114923faf4cb286756e8cb6abca23',
         permanent: false
+      },
+      {
+        source: '/gild',
+        destination: 'https://muzyk0.notion.site/84a114923faf4cb286756e8cb6abca23',
+        permanent: false
       }
     ];
   }
